878bbbb003c80789b4e00f8b9b91645c35c14cd4
Ameer <ameerapex496@gmail.com>
2023-12-04 03:31:53 +0100

    cleaned code + added comments + orginaized code

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// selecting our elements
 var header = document.getElementById("header");
 var title = document.getElementById("title");
 var excerpt = document.getElementById("excerpt");
@@ -6,8 +7,9 @@ var name = document.getElementById("name");
 var date = document.getElementById("date");
 var animated_bgs = document.querySelectorAll(".animated-bg");
 var animated_bg_texts = document.querySelectorAll(".animated-bg-text");
-setTimeout(getData, 2500);
+// our function
 function getData() {
+    // here we are inserting our data to our info card
     header.innerHTML =
         '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
     title.innerHTML = "Lorem ipsum dolor sit amet";
@@ -17,6 +19,9 @@ function getData() {
         '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
     name.innerHTML = "John Doe";
     date.innerHTML = "Oct 08, 2020";
+    // here we remove the loading animation one by one
     animated_bgs.forEach(function (bg) { return bg.classList.remove("animated-bg"); });
     animated_bg_texts.forEach(function (bg) { return bg.classList.remove("animated-bg-text"); });
 }
+// our set time out
+setTimeout(getData, 2500);
--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,16 @@
+// selecting our elements
 const header = document.getElementById("header");
 const title = document.getElementById("title");
 const excerpt = document.getElementById("excerpt");
 const profile_img = document.getElementById("profile_img");
 const name = document.getElementById("name");
 const date = document.getElementById("date");
-
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
-setTimeout(getData, 2500);
-
+// our function
 function getData() {
+  // here we are inserting our data to our info card
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
   title.innerHTML = "Lorem ipsum dolor sit amet";
@@ -21,6 +21,10 @@ function getData() {
   name.innerHTML = "John Doe";
   date.innerHTML = "Oct 08, 2020";
 
+  // here we remove the loading animation one by one
   animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
   animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
 }
+
+// our set time out
+setTimeout(getData, 2500);
